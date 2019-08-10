2421a9d66ad8a1c9745b0251e50a86352fcba7e6
Dinesh <Dinesh@LAPTOP-1RJDNOQU>
2019-08-10 12:21:54 +0530

    Comiting code  to github

---
+++ dbs-hackthon/src/main/java/com/dbs/demo/model/DbsMessage.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.dbs.demo.model;
+
+/**
+ * @author Dinesh
+ *
+ */
+public class DbsMessage {
+
+	
+	public String message;
+	
+	public int qId;
+
+	
+	
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getqId() {
+		return qId;
+	}
+
+	public void setqId(int qId) {
+		this.qId = qId;
+	}
+	
+	
+	
+	
+	
+}
---
+++ dbs-hackthon/src/main/java/com/dbs/demo/model/DbsQueue.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.dbs.demo.model;
+
+/**
+ * @author Dinesh
+ *
+ */
+public class DbsQueue {
+
+	
+	private int queueId;
+	
+	private String queueName;
+
+	public String getQueueName() {
+		return queueName;
+	}
+
+	public void setQueueName(String queueName) {
+		this.queueName = queueName;
+	}
+
+	public int getQueueId() {
+		return queueId;
+	}
+
+	public void setQueueId(int queueId) {
+		this.queueId = queueId;
+	}
+
+	
+	
+	
+	
+	
+	
+}
